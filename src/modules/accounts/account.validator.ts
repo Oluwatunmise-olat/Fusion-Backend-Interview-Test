715af9375f32fab0319c9c794ea8326d235ac106
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-06-25 12:17:31 +0100

    fix bug in account validator

--- src/modules/accounts/account.validator.ts
+++ src/modules/accounts/account.validator.ts
@@ -4,7 +4,6 @@ export class AccountValidator {
   static schema() {
     return Joi.object({
       amount: Joi.number().required(),
-      transaction_ref: Joi.string().required(),
       description: Joi.string(),
     }).options({ allowUnknown: true });
   }
