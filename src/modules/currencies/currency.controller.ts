40460632e648e82555d6ecfe2e336e89ce66a128
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-06-25 12:06:56 +0100

    fix bug in get single currency

--- src/modules/currencies/currency.controller.ts
+++ src/modules/currencies/currency.controller.ts
@@ -17,8 +17,8 @@ export class CurrencyController {
 
   static async getOne(req: Request, res: Response, next: NextFunction) {
     try {
-      const { currency_id } = req.body;
-      const resp = await currencyService.getOne(currency_id);
+      const { currency_Id } = req.params;
+      const resp = await currencyService.getOne(parseInt(currency_Id));
       const { message, data } = resp;
       return res.status(httpStatus.OK).json(apiResponse.success(message, data));
     } catch (error) {
